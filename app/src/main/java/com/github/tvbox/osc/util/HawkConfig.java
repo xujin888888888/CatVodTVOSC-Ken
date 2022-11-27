3b9ffde1a9d30f5d27bb95e86423de5f810be3b7
xujin888888888 <106670190+xujin888888888@users.noreply.github.com>
2022-11-27 15:41:28 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -7,7 +7,10 @@ package com.github.tvbox.osc.util;
  */
 public class HawkConfig {
     public static final String API_URL = "api_url";
+    public static final String EPG_URL = "epg_url";
+    public static final String SHOW_PREVIEW = "show_preview";
     public static final String API_HISTORY = "api_history";
+    public static final String EPG_HISTORY = "epg_history";
     public static final String HOME_API = "home_api";
     public static final String DEFAULT_PARSE = "parse_default";
     public static final String DEBUG_OPEN = "debug_open";
@@ -19,20 +22,19 @@ public class HawkConfig {
     public static final String PLAY_TIME_STEP = "play_time_step"; //0 texture 2
     public static final String DOH_URL = "doh_url";
     public static final String HOME_REC = "home_rec"; // 0 豆瓣热播 1 数据源推荐 2 历史
+    public static final String HISTORY_NUM = "history_num";
     public static final String SEARCH_VIEW = "search_view"; // 0 列表 1 缩略图
-    public static final String THIRD_PARTY_PLAYER = "third_party_player"; // 0 列表 1 缩略图
     public static final String LIVE_CHANNEL = "last_live_channel_name";
     public static final String LIVE_CHANNEL_REVERSE = "live_channel_reverse";
     public static final String LIVE_CROSS_GROUP = "live_cross_group";
     public static final String LIVE_CONNECT_TIMEOUT = "live_connect_timeout";
     public static final String LIVE_SHOW_NET_SPEED = "live_show_net_speed";
     public static final String LIVE_SHOW_TIME = "live_show_time";
-    public static final String FORGET_NEW_VERSION = "forget_new_version";
-    public static final String DISPLAY_LOADING_SPEED = "display_loading_speed";
-    public static final String HOME_VIEW_STYLE = "home_view_style";
-    public static final String ENABLE_2K_ADAPTER = "2k_adapter";
-    public static final String CUSTOM_WALLPAPER = "custom_wallpaper";
-    public static final String STORAGE_DRIVE_SORT = "storage_drive_sort";
-    public static final String TV_TYPE = "tv_type";
-    public static final String REMOTE_CONTROL = "remote_control";
+    public static final String FAST_SEARCH_MODE = "fast_search_mode";
+    public static final String SUBTITLE_TEXT_SIZE = "subtitle_text_size";
+    public static final String SUBTITLE_TIME_DELAY = "subtitle_time_delay";
+    public static final String SOURCES_FOR_SEARCH = "checked_sources_for_search";
+    public static final String HOME_REC_STYLE = "home_rec_style";
+    public static final String NOW_DATE = "now_date"; //当前日期
+    public static final String REMOTE_TVBOX = "remote_tvbox_host";
 }
